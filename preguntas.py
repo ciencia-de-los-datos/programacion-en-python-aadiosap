cf2accfbc3a8a51a2661af7c4f7cf171f3558982
aadiosap <aadiosap@unal.edu.co>
2022-05-19 19:27:37 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -45,13 +45,13 @@ def pregunta_02():
     """
     
     from collections import Counter
+    x = open("C:\data.csv", "r").readlines()
     col_1=[z[0]for z in x]
     col_1
     cuenta_unicos=Counter(col_1)
     cuenta_unicos
     y=[(k,v)for k,v in cuenta_unicos.items()]
-    sorted(y)
-    return
+    return sorted(y)
 
 
 def pregunta_03():
@@ -69,11 +69,15 @@ def pregunta_03():
     ]
 
     """
-   
-    
-    
-    
-    return
+    x = open("C:\data.csv", "r").readlines()
+    from collections import Counter
+    col_1_2=[(z[0],int(z[1])) for z in x]
+    col_1_2
+    contador=Counter()
+    for k,v in col_1_2:
+        contador[k]+=v
+        
+    return sorted(list(contador.items()))
 
 
 def pregunta_04():
@@ -98,9 +102,14 @@ def pregunta_04():
     ]
 
     """
-    
-    
-    return
+    x = open("C:\data.csv", "r").readlines()
+    from collections import Counter
+    mes_segundacol=[(z[2].split("-")[1],int(z[1])) for z in x]
+    mes_segundacol
+    contador2=Counter()
+    for k,v in mes_segundacol:
+        contador2[k]+=1   
+    return sorted(list(contador2.items()))
 
 
 def pregunta_05():
@@ -118,7 +127,30 @@ def pregunta_05():
     ]
 
     """
-    return
+
+    x = open("C:\data.csv", "r").readlines()
+    lista_p5=[]
+    lista_p5_2=[]
+    respuesta_5=[]
+    respuesta_5=[]
+    respuesta_final_5=[]
+    col_1_2=[(z[0],int(z[1])) for z in x]
+    col_1_2[0][0]
+
+    for i in col_1_2:
+        if i[0] not in lista_p5:
+            lista_p5.append(i[0])
+            lista_p5_2=[]
+            for j in col_1_2:
+                if j[0]==i[0]:
+                    lista_p5_2.append(int(j[1]))
+            lista_p5.append(lista_p5_2)
+    respuesta_5=list(((lista_p5[0:2]),(lista_p5[2:4]),(lista_p5[4:6]),(lista_p5[6:8]),(lista_p5[8:10])))
+   
+    for i in respuesta_5:
+        respuesta_final_5.append(([(i[0]),max(i[1]),min(i[1])]))
+
+    return tuple(sorted(respuesta_final_5))
 
 
 def pregunta_06():
@@ -143,7 +175,60 @@ def pregunta_06():
     ]
 
     """
-    return
+    x = open("C:\data.csv", "r").readlines()
+    lista_p6=[]
+    dict_p6={}
+    minimo=0
+    maximo=0
+    lista_letras=[]
+    lista_numeros=[]
+    lista_aux=[]
+    lista_aux2=[]
+    lista_aux3=[]
+    respuesta_p6_2=[]
+
+    col_4=[[y for y in z if ":" in y] for z in x]
+    col_4=[[z[:-1] if "\n" in z else z for z in x] for x in col_4]
+    col_4
+
+    letras=[[y[0:3]for y in z] for z in col_4]
+    letras
+
+    for i in letras:
+        for j in i:
+            lista_letras.append(j)
+    lista_letras=list(sorted(set(lista_letras)))
+    lista_letras
+
+    numeros=[[y for y in z] for z in col_4]
+    numeros
+
+    for i in numeros:
+        for j in i:
+            lista_numeros.append(j)
+    lista_numeros=list(sorted(set(lista_numeros)))
+    lista_numeros
+
+    for i in lista_letras:
+        lista_aux=[]
+        lista_aux2=[]
+        for j in lista_numeros:
+            if j[0:3] == i:
+                lista_aux.append(int(j[4:]))
+                lista_aux2=(i,lista_aux)
+        lista_p6.append(lista_aux2)
+    lista_p6
+
+    for i in lista_p6:
+        dict_p6[i[0]]=(min(i[1]),max(i[1]))
+    respuesta_p6=list(dict_p6.items())
+    respuesta_p6
+
+    for i in respuesta_p6:
+        lista_aux3=(i[0],i[1][0],i[1][1])
+    respuesta_p6_2.append((lista_aux3))
+    
+    return respuesta_p6_2
 
 
 def pregunta_07():
@@ -167,7 +252,27 @@ def pregunta_07():
     ]
 
     """
-    return
+    x = open("C:\data.csv", "r").readlines()
+    dict_p7={}
+    lista_aux=[]
+    lista_aux2=[]
+
+    col_1=[z[1]for z in x]
+    col_1
+
+    col_1_0=[[z[1],z[0]]for z in x]
+    col_1_0
+
+    for i in col_1:
+        for a in col_1_0:
+            dict_p7[i]={}
+            if a[0]==i:
+                if a[1] not in list(dict_p7[i].values()):
+                    lista_aux.append(a[1])
+        dict_p7[i]=lista_aux
+        lista_aux=[]
+    
+    return list(tuple(list(dict_p7.items())))
 
 
 def pregunta_08():
@@ -192,7 +297,27 @@ def pregunta_08():
     ]
 
     """
-    return
+    x = open("C:\data.csv", "r").readlines()
+    dict_p8={}
+    lista_aux=[]
+    lista_aux2=[]
+
+    col_1=[z[1]for z in x]
+    col_1
+
+    col_1_0=[[z[1],z[0]]for z in x]
+    col_1_0
+
+    for i in col_1:
+        for a in col_1_0:
+            dict_p8[i]={}
+            if a[0]==i:
+                if a[1] not in list(dict_p8[i].values()):
+                    lista_aux.append(a[1])
+        dict_p8[i]=sorted(list(set(lista_aux)))
+        lista_aux=[]
+    
+    return list(tuple(list(sorted(dict_p8.items()))))
 
 
 def pregunta_09():
@@ -215,7 +340,34 @@ def pregunta_09():
     }
 
     """
-    return
+    x = open("C:\data.csv", "r").readlines()
+    lista_p9=[]
+    lista_p9_2=[]
+    dict_p9={}
+    cuenta=0
+    lista_aux9=[]
+
+    col_4=[[y for y in z if ":" in y] for z in x]
+    col_4=[[y.split(",") for y in z] for z in col_4]
+    col_4=[[[y[:-1] if "\n" in y else y for y in x] for x in z]for z in col_4]
+    col_4=[[[y[0:3] for y in x] for x in z]for z in col_4]
+    col_4
+
+    for i in col_4:
+        lista_p9 = lista_p9+ i
+    lista_p9
+
+    for j in lista_p9:
+        lista_p9_2 = lista_p9_2+ j
+    lista_p9_2
+
+    for c in lista_p9_2:
+        if c in list(dict_p9.keys()):
+            dict_p9[c]=dict_p9[c]+1
+        else:
+            dict_p9[c]=1
+
+    return dict_p9
 
 
 def pregunta_10():
@@ -236,7 +388,24 @@ def pregunta_10():
 
 
     """
-    return
+    x = open("C:\data.csv", "r").readlines()
+    col_1=[z[0] for z in x]
+    col_1
+
+    col_4=[z[3:7] for z in x]
+    col_4=[[y for y in z if ":" not in y] for z in col_4]
+    col_4=[len(z) for z in col_4]
+    col_4
+
+    col_5=[z[5:]for z in x]
+    col_5=[[o for o in z if ":" in o] for z in col_5]
+    col_5=[[o[:-1] if "\n" in o else o for o in z]for z in col_5]
+    col_5=[len(z) for z in col_5]
+    col_5
+
+    resultado=zip(col_1,col_4,col_5)
+    
+    return list(resultado)
 
 
 def pregunta_11():
@@ -257,8 +426,26 @@ def pregunta_11():
 
 
     """
-    return
+    x = open("C:\data.csv", "r").readlines()
+    dict_p11={}
+    contador=0
 
+    col_p11=[z[1:7] for z in x]
+    col_p11=[[y for y in z if ":" not in y] for z in col_p11]
+    col_p11=[[y for y in z if "-" not in y] for z in col_p11]
+    col_p11=[[z[0],z[1:]] for z in col_p11]
+    col_p11
+
+
+    for i in col_p11:
+        contador=int(i[0])
+        for j in i[1]:
+            if j in dict_p11:
+                dict_p11[j]+=contador
+            else:
+                dict_p11[j]=contador     
+    
+    return dict_p11
 
 def pregunta_12():
     """
@@ -275,4 +462,34 @@ def pregunta_12():
     }
 
     """
-    return
+    x = open("C:\data.csv", "r").readlines()
+    lista_p12=[]
+    contador=0
+    dict_p12={}
+
+    col_1=[z[0] for z in x]
+    col_1
+
+    col_5=[z[5:]for z in x]
+    col_5=[[o for o in z if ":" in o] for z in col_5]
+    col_5=[[o[:-1] if "\n" in o else o for o in z]for z in col_5]
+    col_5=[[o[4:] for o in z]for z in col_5]
+    col_5
+
+    for i in col_5:
+        contador=0
+        for j in i:
+            contador+=int(j)
+        lista_p12.append(contador)
+    lista_p12
+
+    lista_p12_f=list(zip(col_1,lista_p12))
+    lista_p12_f
+
+    for a,b in lista_p12_f:
+        if a in dict_p12:
+            dict_p12[a]+=int(b)
+        else:
+            dict_p12[a]=int(b)
+    
+    return dict_p12
